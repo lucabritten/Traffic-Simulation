0b5453e953ec0a67b50035974e43f7100444fd9f
Luca Britten <lucabritten17@icloud.com>
2025-12-05 12:44:45 +0100

    Added validation and comprehensive unit tests for `Vehicle`. Enhanced `Vehicle` with position tracking and safety checks for position, speed, and road changes.

--- src/main/java/com/britten/domain/Vehicle.java
+++ src/main/java/com/britten/domain/Vehicle.java
@@ -13,6 +13,7 @@ public abstract class Vehicle {
         this.id = id;
         this.currentRoad = currentRoad;
         this.speed = speed;
+        this.position = 0;
     }
 
     public int getId() {
@@ -32,14 +33,24 @@ public abstract class Vehicle {
     }
 
     public void setPosition(int position){
+        if(position < this.position)
+            throw new IllegalArgumentException("A vehicle is not allowed to drive backwards!");
+        if(position > currentRoad.getLength())
+            throw new IllegalArgumentException("Value position is greater than length of current road.");
+
         this.position = position;
     }
 
     public void setCurrentRoad(Road currentRoad){
+        if(currentRoad == null)
+            throw new IllegalArgumentException("Road cannot be null.");
         this.currentRoad = currentRoad;
     }
 
     public void setSpeed(double speed){
+        if(speed < 0)
+            throw new IllegalArgumentException("Speed cannot be negative.");
+
         this.speed = speed;
     }
 
--- src/test/java/com/britten/domain/VehicleTest.java
+++ src/test/java/com/britten/domain/VehicleTest.java
@@ -22,6 +22,7 @@ public class VehicleTest {
         assertThat(vehicle.getId()).isEqualTo(id);
         assertThat(vehicle.getSpeed()).isEqualTo(CAR_SPEED);
         assertThat(vehicle.getCurrentRoad()).isEqualTo(road);
+        assertThat(vehicle.getPosition()).isZero();
     }
 
     @Test
@@ -33,4 +34,93 @@ public class VehicleTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Current road cannot be null!");
     }
+
+    @Test
+    void testSetPosition_validValue_updatePosition(){
+        Road road = new Road(
+                new Intersection(1, new TrafficLight(1,1,1)),
+                new Intersection(2,new TrafficLight(1,1,1)),
+                100);
+        Vehicle vehicle = new Car(1, road);
+
+        vehicle.setPosition(5);
+
+        assertThat(vehicle.getPosition()).isEqualTo(5);
+    }
+
+    @Test
+    void testSetPosition_smallerValue_throwException(){
+        Road road = new Road(
+                new Intersection(1, new TrafficLight(1,1,1)),
+                new Intersection(2,new TrafficLight(1,1,1)),
+                100);
+        Vehicle vehicle = new Car(1, road);
+
+        vehicle.setPosition(10);
+
+        assertThatThrownBy(() -> vehicle.setPosition(5))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("A vehicle is not allowed to drive backwards!");
+    }
+
+    @Test
+    void testSetPosition_valueGreaterRoadLength_throwException(){
+        Road road = new Road(
+                new Intersection(1, new TrafficLight(1,1,1)),
+                new Intersection(2,new TrafficLight(1,1,1)),
+                100);
+        Vehicle vehicle = new Car(1, road);
+
+        int position = vehicle.getCurrentRoad().getLength() + 1;
+
+        assertThatThrownBy(() -> vehicle.setPosition(position))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Value position is greater than length of current road.");
+    }
+
+    @Test
+    void testSetCurrentRoad_validRoad_accept(){
+        Road road1 = new Road(
+                new Intersection(1, new TrafficLight(1,1,1)),
+                new Intersection(2,new TrafficLight(1,1,1)),
+                100);
+        Vehicle vehicle = new Car(1, road1);
+
+        Road road2 = new Road(
+                new Intersection(3, new TrafficLight(1,1,1)),
+                new Intersection(4,new TrafficLight(1,1,1)),
+                100);
+
+        vehicle.setCurrentRoad(road2);
+
+        assertThat(vehicle.getCurrentRoad()).isEqualTo(road2);
+    }
+
+    @Test
+    void testSetCurrentRoad_nullRoad_throwException(){
+        Road road = new Road(
+                new Intersection(1, new TrafficLight(1,1,1)),
+                new Intersection(2,new TrafficLight(1,1,1)),
+                100);
+        Vehicle vehicle = new Car(1, road);
+
+        assertThatThrownBy(() -> vehicle.setCurrentRoad(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Road cannot be null.");
+    }
+
+    @Test
+    void testSetSpeed_negativeValue_throwException(){
+        Road road = new Road(
+                new Intersection(1, new TrafficLight(1,1,1)),
+                new Intersection(2,new TrafficLight(1,1,1)),
+                100);
+        Vehicle vehicle = new Car(1, road);
+
+        assertThatThrownBy(() -> vehicle.setSpeed(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Speed cannot be negative.");
+    }
 }
+
+
